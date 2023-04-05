17bf39f664f71af337452dbe68f2378be99187bd
n.semenchuk <n.semenchuk@itransition.com>
2023-04-05 22:33:14 +0300

    add tiny editor

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,8 +2,8 @@ module.exports = {
   '/src/**/*.js': 'eslint --config .eslintrc.js --fix',
   '**/*.json': 'prettier --write',
   '**/*.md': 'prettier --write',
-  // '**/*.ts?(x)': [
-  //   () => 'tsc -p tsconfig.json --noEmit',
-  //   'eslint --config .eslintrc.js --fix',
-  // ],
+  '/src/**/*.ts?(x)': [
+    () => 'tsc -p tsconfig.json --noEmit',
+    'eslint --config .eslintrc.js --fix',
+  ],
 }
